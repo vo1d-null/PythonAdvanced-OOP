b1a28b05a4b97ef7b8fb21167615b394598927b8
Nikolay Gerginov <123015737+vo1d-null@users.noreply.github.com>
2023-05-25 01:38:44 +0300

    Update 01Flatten_Lists.py
    -optimized solution 01

--- 04MultiDimensional_Lists_EXC_2/01Flatten_Lists.py
+++ 04MultiDimensional_Lists_EXC_2/01Flatten_Lists.py
@@ -1,7 +1,10 @@
-line = input().split('|')
+# line = input().split('|')
+#
+# sub_lists = []
+# for sub_string in line[::-1]:
+#     sub_lists.extend(sub_string.split())
+#
+# print(*sub_lists)
 
-sub_lists = []
-for sub_string in line[::-1]:
-    sub_lists.extend(sub_string.split())
-
-print(*sub_lists)
+nums = [string.split() for string in input().split('|')]
+print(*[' '.join(sub_list) for sub_list in nums[::-1]])
