280bca87cbb5d883509f0fb4566bc20a8c33a037
Nikolay Gerginov <123015737+vo1d-null@users.noreply.github.com>
2023-06-01 20:34:41 +0300

    -Added 04
    -Finished 03
    -Added 04

--- 07_File_Handling_EXC/03File_Manipulator.py
+++ 07_File_Handling_EXC/03File_Manipulator.py
@@ -1,14 +1,28 @@
+import os
+
 while True:
     command, *info = input().split('-')
 
     if command == 'Create':
-        with open(f'files/{info[0]}', 'w') as file:
-            file.write(info[1])
+        with open(f'files/{info[0]}', 'w') as file: pass
     elif command == 'Add':
-        pass
+        with open(f'files/{info[0]}', 'a') as file:
+            file.write(f'{info[1]}\n')
     elif command == 'Replace':
-        pass
+        try:
+            with open(f'files/{info[0]}', 'r+') as file:
+                text = file.read()
+                text = text.replace(info[1], info[2])
+
+                file.seek(0)
+                file.write(text)
+
+        except FileNotFoundError:
+            print('An error occurred')
     elif command == 'Delete':
-        pass
+        try:
+            os.remove(f'files/{info[0]}')
+        except FileNotFoundError:
+            print('An error occurred')
     elif command == 'End':
         break
