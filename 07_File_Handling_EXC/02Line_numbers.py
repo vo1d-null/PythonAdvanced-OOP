e592f4e2562d33f10121486fadc26e1da15898d6
Nikolay Gerginov <123015737+vo1d-null@users.noreply.github.com>
2023-06-01 19:49:21 +0300

    Update 02Line_numbers.py
    Optimized

--- 07_File_Handling_EXC/02Line_numbers.py
+++ 07_File_Handling_EXC/02Line_numbers.py
@@ -3,18 +3,14 @@ from string import punctuation
 with open('01text.txt', 'r') as file:
     text = file.readlines()
 
-output_file = open('01output.txt', 'w')
+with open('01output.txt', 'w') as output_file:
+    for i in range(len(text)):
+        letters, marks = 0, 0
 
-for i in range(len(text)):
-    letters, marks = 0, 0
+        for symbol in text[i]:
+            if symbol.isalpha():
+                letters += 1
+            elif symbol in punctuation:
+                marks += 1
 
-    for symbol in text[i]:
-        if symbol.isalpha():
-            letters += 1
-        elif symbol in punctuation:
-            marks += 1
-
-    output_file.write(f'Line {i + 1}: {"".join(text[i][:-1])}({letters})({marks})\n')
-
-
-output_file.close()
+        output_file.write(f'Line {i + 1}: {"".join(text[i][:-1])}({letters})({marks})\n')
