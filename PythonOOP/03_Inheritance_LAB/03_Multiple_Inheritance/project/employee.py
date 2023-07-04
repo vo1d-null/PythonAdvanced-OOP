23b885522ee5919c172630d647a3dcd317676205
Nikolay Gerginov <123015737+vo1d-null@users.noreply.github.com>
2023-07-04 06:48:56 -0700

    added solutions to LAB
    Added solutions for lab

---
+++ PythonOOP/03_Inheritance_LAB/03_Multiple_Inheritance/project/employee.py
@@ -0,0 +1,4 @@
+class Employee:
+    def get_fired(self):
+        # Return a string indicating that the employee has been fired
+        return "fired..."
---
+++ PythonOOP/03_Inheritance_LAB/03_Multiple_Inheritance/project/person.py
@@ -0,0 +1,5 @@
+class Person:
+    def sleep(self):
+        # Return the string "sleeping..."
+        return "sleeping..."
+    
